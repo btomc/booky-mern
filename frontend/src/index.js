fe6c6c36850cc14844aa7e114c39d33840efffb2
Beata Tomczak <tma.btty@gmail.com>
2021-02-07 15:17:54 +0100

    Create a redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import GlobalStyle from './globalStyles'
 import App from './App';
 
 ReactDOM.render(
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <App />
-    </>
-    ,
+    </Provider>,
   document.getElementById('root')
 );
 
