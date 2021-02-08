e30747c99401c9234cd72b397ce1563d0a7aeb1b
Beata Tomczak <tma.btty@gmail.com>
2021-02-08 20:54:54 +0100

    User authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,8 +4,6 @@ import { getProducts, getProductById } from '../controllers/productController.js
 
 
 router.route('/').get(getProducts)
-
-
 router.route('/:id').get(getProductById)
 
 export default router
