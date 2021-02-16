1598b56f397276e8e3fab730069ba8e9ffa3c6ab
Beata Tomczak <tma.btty@gmail.com>
2021-02-16 23:46:23 +0100

    Admin middleware

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -21,7 +21,7 @@ const MessageContainer = styled.div`
 const MessageText = styled.p`
     font-size: 1.1rem;
     padding: .8rem;
-    background: ${({ variant }) => variant ? '#814cad' : '#e84040'};
+    background: ${({ variant }) => variant ? '#23208c' : '#e84040'};
     color: #fff;
     border-radius: 4px;
     font-weight: 700;
